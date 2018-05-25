a045ad3ab00cf278900ce39c8557aea6cd031cf4
David Litvak <david.litvakb@gmail.com>
2018-05-25 23:23:22 +0200

    Fix API Key updates

--- tests/api_key_test.py
+++ tests/api_key_test.py
@@ -1,5 +1,6 @@
 import vcr
 from unittest import TestCase
+from contentful_management.resource import Link
 from contentful_management.api_key import ApiKey
 from contentful_management.errors import NotFoundError
 from .test_helper import CLIENT, PLAYGROUND_SPACE
@@ -142,3 +143,26 @@ class ApiKeyTest(TestCase):
         with vcr.use_cassette('fixtures/api_key/find_preview.yaml'):
             preview_api_key = api_key.preview_api_key()
             self.assertEqual(str(preview_api_key), "<PreviewApiKey[management.py - playground 1] id='5mytqWZjcqEWMIHVfe5cUi' access_token='PREVIEW_TOKEN'>")
+
+    @vcr.use_cassette('fixtures/api_key/find_3.yaml')
+    def test_update_api_key_with_new_environment(self):
+        api_key = CLIENT.api_keys(PLAYGROUND_SPACE).find('5mxNhKOZYOp1wzafOR9qPw')
+
+        with vcr.use_cassette('fixtures/api_key/update_key.yaml'):
+            self.assertEqual(len(api_key.environments), 1)
+            self.assertEqual(api_key.environments[0].id, 'testing')
+
+            api_key.environments.append(
+                Link({
+                    'sys': {
+                        'id': 'master',
+                        'type': 'Link',
+                        'linkType': 'Environment'
+                    }
+                })
+            )
+
+            api_key.save()
+
+            self.assertEqual(len(api_key.environments), 2)
+
