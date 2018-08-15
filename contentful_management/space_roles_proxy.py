e0054de896495e82c6795a777e6b0f52eecbb925
David Litvak <david.litvakb@gmail.com>
2018-08-15 11:18:50 +0200

    Fix Space Roles Proxy requiring extra argument

--- contentful_management/space_roles_proxy.py
+++ contentful_management/space_roles_proxy.py
@@ -22,3 +22,10 @@ class SpaceRolesProxy(SpaceResourceProxy):
 
     def _resource_proxy_class(self):
         return RolesProxy
+
+    def create(self, attributes=None, **kwargs):
+        """
+        Creates a role with given attributes.
+        """
+
+        return super(SpaceRolesProxy, self).create(None, attributes)
